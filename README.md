3dcc83f5467d3cca3935dfadcda0dc3800422c7f
WangXuan95 <629708558@qq.com>
2025-06-02 00:51:02 +0800

    use ImCvt to quantize

--- README.md
+++ README.md
@@ -4,19 +4,28 @@
 
 一个 Python 程序，帮助你缩小 PNG 图像文件的大小 (类似 [tinypng.com](https://tinypng.com/)) 。优点有：
 
-- 效果更好：相比 [tinypng.com](https://tinypng.com/) ，同样的画质下压缩率更高。
 - 可调：通过调整参数，用画质换取更高的压缩率。
 - 摆脱 [tinypng.com](https://tinypng.com/) 的文件数量限制。
+
+| ![img_uncompressed](image/img01.png) | ![img_256](image_tiny/img01_256.png) |
+| :----------------------------------: | :----------------------------------: |
+|           原图，426530字节           |        256色压缩，128154字节         |
+|  ![img_99](image_tiny/img01_99.png)  |  ![img_16](image_tiny/img01_16.png)  |
+|         99色压缩，97921字节          |         16色压缩，54192字节          |
 
 　
 
 ## 原理简介
 
-### 色域量化
+#### 色域量化
 
-与 [tinypng.com](https://tinypng.com/) 类似，本代码将色彩数=16777216 的24位真彩色像素量化为色彩数≤256的调色板像素，从而达到大于 3 倍的压缩比。不同的是，本代码用 K-Means 聚类算法进行量化。
+与 [tinypng.com](https://tinypng.com/) 类似，本代码将色彩数=16777216 的24位真彩色像素量化为色彩数≤256的调色板像素，并使用 PNG 标准规定的调色板(palette) 模式 (color_type=3) 来存储量化后的图像，从而达到大于 3 倍的压缩比。
 
-### Deflate 极限压缩
+考虑到 PNG 的 color_type=3 模式只支持 RGB 调色板，本代码只能支持 RGB 图像压缩，对于 RGBA 图像，会先将它转化为 RGB 图像（alpha通道会丢失）。
+
+本代码调用 [ImCvt](https://github.com/WangXuan95/ImCvt) v0.6 来将图像转化为色域量化模式的 PNG ，它使用 K-Means 聚类算法进行量化。
+
+#### Deflate 极限压缩
 
 本代码还调用了 [OptiPNG](https://optipng.sourceforge.net/) 。它会重新对 PNG 的 Deflate 压缩过程进行优化，从而达到更大的压缩比。
 
@@ -24,20 +33,23 @@
 
 ## 安装依赖
 
-需要安装 Python 3.x 以及其配套的 Pillow (PIL) 、 numpy 、 scipy 、scikit-learn (sklearn) 库。
+#### 安装 Python
+
+需要安装 Python 3.x 以及其配套的 Pillow (PIL) 、 numpy 库。
 
 如果你已有 Python 3.x ，运行以下命令安装这些库：
 
 ```powershell
 python -m pip install Pillow==8.4.0
 python -m pip install numpy==1.20.3
-python -m pip install scipy==1.8.0
-python -m pip install scikit-learn==0.24.2
 ```
 
 如果你还没有 Python ，你可以直接安装 [Anaconda](https://www.anaconda.com/products/distribution) ，它包含 python 和上述库。
 
-另外，在使用时，要保证 `optipng.exe` 与 `tinypng.py` 在同一个目录下
+#### 安装第三方工具
+
+- 如果你的系统是Windows，要保证 `ImCvt.exe` 和 `optipng.exe` 与 `tinypng.py` 在同一个目录下。
+- 如果你的系统是 Linux ，需要前往链接 https://github.com/WangXuan95/ImCvt 编译 ImCvt；前往链接 https://optipng.sourceforge.net/ 编译 OptiPNG ，并将编译产生的 Linux 可执行文件 `ImCvt` 和 `optipng` 放在与 `tinypng.py` 在同一个目录下。
 
 　
 
@@ -49,7 +61,7 @@ python -m pip install scikit-learn==0.24.2
 python tinypng.py <输入目录名> <输出目录名> <P>
 ```
 
-它会把输入目录中的所有图像文件 (包括 png, jpg, tiff 等各种图像格式) 压缩成 png 格式，放在输出目录中 (对于一些本身就高度压缩的 jpg 图像，生成的 png 会比原始文件都大，程序会跳过这种文件) 。**P值**则是**色彩数**，取值范围是 `P∈[2,256]` ，越小则压缩率越高，图像失真也越大。
+它会把输入目录中的所有图像文件 (包括 png, jpg, tiff 等各种图像格式) 压缩成 png 格式，放在输出目录中。**P值**则是**色彩数**，取值范围是 `P∈[2,256]` ，越小则压缩率越高，图像失真也越大。
 
 比如用以下命令来压缩 [image](./image) 目录中的图像，放到 [image_tiny]() 目录中，`P=100` ：
 
@@ -61,20 +73,11 @@ python tinypng.py image image_tiny 100
 
 　
 
-## 效果展示
+## 失真评估
 
-对 [image](./image) 目录中的图像，分别用 [tinypng.com](https://tinypng.com/) 和本代码 (取几种**不同P值**) 进行压缩，下表部分展示了总压缩率和平均失真。其中压缩率=原始图像文件大小/压缩后图像文件大小) 。而失真用 SSIM 值 (结构相似性) 来衡量，SSIM∈[0.0, 1.0] ，越大说明相似度越高，失真越小。
+我们可以通过 SSIM 来评估量化压缩后的图像相比原始图像的失真。
 
-   表： [tinypng.com](https://tinypng.com/) 和本代码的对比
-
-| 文件名            | [tinypng.com](https://tinypng.com/) | 本代码 P=256 | 本代码 P=150 | 本代码 P=110 | 本代码 P=50 |
-| ----------------- | :---------------------------------: | :----------: | :----------: | :----------: | :---------: |
-| **压缩率**        |                2.71                 |     2.88     |     3.37     |     3.69     |    4.93     |
-| **画质 (SSIM值)** |               0.9770                |    0.9865    |    0.9812    |    0.9773    |   0.9647    |
-
-从上表可以明显看出本代码相比 [tinypng.com](https://tinypng.com/) 的优势：在相同压缩率下图像失真更小，在相同失真下压缩率更高。
-
-我也提供了计算 SSIM 的代码文件 ssim.py ，运行方法是：
+这里提供了计算 SSIM 的代码文件 ssim.py ，运行方法是：
 
 ```powershell
 python ssim.py <原始图像目录> <原始图像格式> <压缩图像目录> <压缩图像格式>
--- tinypng.py
+++ tinypng.py
@@ -4,20 +4,18 @@
 import os
 import sys
 import warnings
-#import subprocess as sp
 import numpy as np
 from PIL import Image
-from sklearn.cluster import MiniBatchKMeans
 
 
-
+IMCVT_EXECUTABLE_FILE_NAME_LINUX     = 'ImCvt'
+IMCVT_EXECUTABLE_FILE_NAME_WINDOWS   = 'ImCvt.exe'
 OPTIPNG_EXECUTABLE_FILE_NAME_LINUX   = 'optipng'
 OPTIPNG_EXECUTABLE_FILE_NAME_WINDOWS = 'optipng.exe'
     
 
-
-def loadImageFileAsRGBArray(fname) :
-    img = Image.open(fname)
+def inputImageAsRGBandSaveAsPPM (src_fname, dst_fname) :
+    img = Image.open(src_fname)
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore')
         if img.mode in ['RGBA'] :
@@ -35,53 +33,7 @@ def loadImageFileAsRGBArray(fname) :
             img_rgb = img.convert('RGB')
             arr_rgb = np.asarray(img_rgb)
     img.close()
-    return arr_rgb
-
-
-
-
-def saveImageAsPalettePNGusingKMeans(quant, img_array2d, fname) :
-    h, w, _ = img_array2d.shape
-    
-    kmeans_n_clusters = min(quant, w*h)
-    
-    img_array1d = img_array2d.reshape((-1,3))
-    
-    with warnings.catch_warnings():
-        warnings.filterwarnings('ignore')
-        kmeans_res = MiniBatchKMeans(n_clusters=kmeans_n_clusters, init='k-means++', max_iter=100, batch_size=32768, compute_labels=True, random_state=0).fit(img_array1d)
-    
-    palette_array = (kmeans_res.cluster_centers_ + 0.5).clip(0.0, 255.9).astype(np.uint8)
-    
-    img_palette_array1d = palette_array[kmeans_res.labels_]
-    
-    img_palette_array2d = img_palette_array1d.reshape(img_array2d.shape)
-    
-    Image.fromarray(img_palette_array2d).convert('P', palette=Image.ADAPTIVE).save(fname, optimize=True)
-    
-    return kmeans_res.n_iter_
-
-
-
-
-def callOptiPNG (optipng_path, dst_fname) :
-    #COMMANDS = [ optipng_path, '-o7', dst_fname ]
-    #p = sp.Popen(COMMANDS, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
-    #p.wait()
-    command = optipng_path + ' -o7 ' + dst_fname
-    print(command)
-    os.system(command)
-
-
-
-
-def copy_file (src_path, dst_path) :
-    if  os.path.isfile(src_path)  and  not os.path.exists(dst_path) :
-        with open(src_path, 'rb') as src_fp :
-            with open(dst_path, 'wb') as dst_fp :
-                dst_fp.write(src_fp.read())
-
-
+    Image.fromarray(arr_rgb).save(dst_fname)
 
 
 if __name__ == '__main__':
@@ -89,120 +41,68 @@ if __name__ == '__main__':
     # judge OS --------------------------------------------------------
     if   sys.platform.lower().startswith('win'):
         print('Windows')
+        imcvt_executable_fname   =   IMCVT_EXECUTABLE_FILE_NAME_WINDOWS
         optipng_executable_fname = OPTIPNG_EXECUTABLE_FILE_NAME_WINDOWS
     elif sys.platform.lower().startswith('linux'):
         print('Linux')
+        imcvt_executable_fname   =   IMCVT_EXECUTABLE_FILE_NAME_LINUX
         optipng_executable_fname = OPTIPNG_EXECUTABLE_FILE_NAME_LINUX
     else:
         print('current OS %s not support' % sys.platform)
-        exit(-1)
+        exit(1)
     
-    
-    # get the path of optipng --------------------------------------------------------
-    work_path = sys.argv[0]
-    work_dir, _  = os.path.split(work_path)
-    
+    # get the path executable files --------------------------------------------------------
+    work_dir, _  = os.path.split(sys.argv[0])
     if work_dir == '' :
-        optipng_path = '.'      + os.path.sep + optipng_executable_fname
+        imcvt_path   = os.path.join('.'     , imcvt_executable_fname)
+        optipng_path = os.path.join(work_dir, optipng_executable_fname)
     else :
-        optipng_path = work_dir + os.path.sep + optipng_executable_fname
-    
+        imcvt_path   = os.path.join('.'     , imcvt_executable_fname)
+        optipng_path = os.path.join(work_dir, optipng_executable_fname)
     
     # parse command args --------------------------------------------------------
     try :
-        src_dir = sys.argv[1]
-        dst_dir = sys.argv[2]
+        src_dir, dst_dir = sys.argv[1:3]
         if len(sys.argv) > 3 :
             quant = int(sys.argv[3])
         else :
-            quant = 256
+            quant = 100
+        quant = max(min(quant, 256), 2)
     except:
         print('Usage: python  %s  <source_directory>  <destination_directory>  [<quant(2~256)>]' % sys.argv[0])
-        exit(-1)
-    
-    if quant < 2:
-        quant = 2
-    if quant > 256:
-        quant = 256
-    
-    
-    # print info --------------------------------------------------------
+        exit(1)
     print('source_directory      = %s' % src_dir )
     print('destination_directory = %s' % dst_dir )
     print('quant                 = %d' % quant   )
     print()
     
-    
     # If dst_dir not exist, create it. If already exist, ask user whether to continue --------------------------------------------------------
     if os.path.isdir(dst_dir) :
         if input('warning: destination_directory %s already exist! continue? (y|n)' % dst_dir) != 'y':
-            exit(-1)
+            exit(1)
     else:
-        os.mkdir(dst_dir)
-    
-    
-    # statistics variables -----------------------------
-    total_count    = 0
-    total_src_size = 0
-    total_dst_size = 0
-    
+        os.makedirs(dst_dir)
     
     # for all files in src_dir, try to compress it, and save to dst_dir --------------------------------------------------------
     for fname in os.listdir(src_dir) :
-        
         fname_prefix, _ = os.path.splitext(fname)
-        dst_fname = dst_dir + os.path.sep + fname_prefix + '.png'
-        src_fname = src_dir + os.path.sep + fname
+        src_fname = os.path.join(src_dir, fname)
+        tmp_fname = os.path.join(dst_dir, (fname_prefix+'.ppm'))
+        dst_fname = os.path.join(dst_dir, (fname_prefix+'.png'))
         
-        # try to open file as image -----------------------------
         try :
-            src_size = os.path.getsize(src_fname)
-            img_array2d = loadImageFileAsRGBArray(src_fname)
+            inputImageAsRGBandSaveAsPPM(src_fname, tmp_fname)
         except :
             print('%s skip' % fname)
             continue
         
-        # get image height and width -----------------------------
-        h, w, _ = img_array2d.shape
+        command = f'{imcvt_path} -{quant} -f {tmp_fname} -o {dst_fname}'
+        print(command)
+        os.system(command)
+        os.remove(tmp_fname)
         
-        # compress stage1 : use KMeans to quantize image and save as palette-PNG file (lossy) -----------------------------
-        print('compressing stage1 (KMeans Quantize) ...')
-        n_iter = saveImageAsPalettePNGusingKMeans(quant, img_array2d, dst_fname)
-        
-        # compress stage2 : use optipng to do an optimization of deflate coding (lossless) -----------------------------
-        print('compressing stage2 (OptiPNG) ...')
-        callOptiPNG (optipng_path, dst_fname)
-        #command = '%s -o7 "%s"' % (optipng_path, dst_fname)
-        #os.system(command)
-        
-        # get compressed PNG size -----------------------------
-        dst_size = os.path.getsize(dst_fname)
-        
-        # decide whether to use the compressed file, or just copy the original file
-        if src_size <= dst_size :                               # not even better than original
-            if src_dir != dst_dir :
-                os.remove(dst_fname)                            # delete it
-            dst_fname = dst_dir + os.path.sep + fname           # construct copy file name
-            copy_file(src_fname, dst_fname)                     # just copy file
-            dst_size = src_size
-            print('%s (%dx%d)   just copy' % (fname, w, h) )
-        else :
-            print('%s (%dx%d)   kmeans-iter=%d   %dB -> %dB   %.2f%% off!' % (fname, w, h, n_iter, src_size, dst_size, 100.0*(1.0-dst_size/src_size) ) )
-        
-        
-        total_count    += 1
-        total_src_size += src_size
-        total_dst_size += dst_size
-    
-    
-    print()
-    if total_count > 0 :
-        print('%d files compressed    %dB -> %dB   %.2f%% off!' % (total_count, total_src_size, total_dst_size, 100.0*(1.0-total_dst_size/total_src_size) ) )
-    else :
-        print('no input file')
-
-
-
-
+        command = f'{optipng_path} -o5 {dst_fname}'
+        print(command)
+        os.system(command)
 
     
